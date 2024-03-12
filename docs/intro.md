3c9f4212a5cb1b7e18f77a14223a3de446b48846
LitCode <Posho19012005@gmail.com>
2024-03-12 16:13:40 -0500

    first commit

---
+++ docs/intro.md
@@ -0,0 +1,32 @@
+---
+sidebar_position: 1
+---
+
+# Introduccion
+
+En esta documentacion el **Grupo de Trabajo F1** en cargado de la parte **Front-End** se encargara de mostrar los errores y las posibles mejoras que se le puede dar la pagina web de la **Tienda Virutal**.
+
+![Tienda Virtual](/img/tienda.png)
+
+## Casos a Resolver 
+
+:::tip
+
+Analizar modulo **Tienda Virtual** Frontend (Diseño, interacción, flujo, UX, UI):
+
+:::
+
+1. Investigar según su área otros sistemas para entender mejor como debería funcionar el modulo seleccionado
+
+2. Documentar errores y nuevos requerimientos en docusaurus
+
+3. Cada error debe estar en un articulo detallando el error y explicando la posible solución y el porque.
+
+4. En el caso de las sugerencias de los nuevos requerimientos, mencionar lo que hay en otro sistema con capturas y detallar como integrarían esa nueva característica o funcionalidad al modulo de su grupo
+
+## Metodos de Trabajo
+
+Creamos un Repositorio en **GitHub** con **Docusaurus** donde en este repositorio estaremos subiendo la **Documentacion** sobre la **Tienda Virtual**:
+
+## Nuesta Documentacion Hosteada con GitHub Pages
+
---
+++ docusaurus.config.js
@@ -0,0 +1,141 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Modulo - Tienda virtual',
+  tagline: 'Guia de Errores | Empresa Buho',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://Docusaurus-Buho',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: 'Buho/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'LitC0d3', // Usually your GitHub org/user name.
+  projectName: 'gh-pages', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/LitC0d3/Docusaurus-Buho',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/LitC0d3/Docusaurus-Buho',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Modulo - Tienda virtual',
+        logo: {
+          alt: 'Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Documentacion',
+          },
+          {to: '/blog', label: 'Colaboradores', position: 'left'},
+          {
+            href: 'https://github.com/LitC0d3/Docusaurus-Buho',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Documentacion',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Repositorio',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/LitC0d3/Docusaurus-Buho',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
