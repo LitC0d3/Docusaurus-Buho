a21ad510c899d3db2c4cfb2b37dff85672585ea9
josephstevetorres <josephtorres568@gmail.com>
2024-03-12 21:10:07 -0500

    Cambios en colaboradores

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,7 +125,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Modulo - Tienda virtual Buho  `,
+        copyright: `Copyright © ${new Date().getFullYear()} Modulo - Tienda virtual Buho`,
       },
       prism: {
         theme: prismThemes.github,
