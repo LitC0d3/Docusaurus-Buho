ac479c768d1e926dfdaca09da2a930b145d5f57e
LitCode <Posho19012005@gmail.com>
2024-03-24 01:32:15 -0500

    Error en la baseurl corregido

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://Docusaurus-Buho',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'Buho/',
+  baseUrl: '/Buho/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
